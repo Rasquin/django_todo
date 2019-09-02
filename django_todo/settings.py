5038d7c04f7998d60fac1961f7a46c6d7b9d24fa
Rasquin <yannymk@hotmail.com>
2019-09-02 13:12:45 +0000

    environment variable in the .bashrc created and setting.py updated with this changes

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -38,11 +38,11 @@ variable.
 
 ALLOWED_HOSTS = ['7a1b23e878be477493fff172cccfd29c.vfs.cloud9.us-east-1.amazonaws.com',
                 'rasquin-django-todo.herokuapp.com']
+                
+                to use the environment variables, yo need to create them 1st in the .bashrc and export them
 """
-#ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'),
-#                os.environ.get('HOSTNAME')]
-ALLOWED_HOSTS = ['7a1b23e878be477493fff172cccfd29c.vfs.cloud9.us-east-1.amazonaws.com',
-                'rasquin-django-todo.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('AWS_C9_HOSTNAME'),
+                os.environ.get('HEROKU_HOSTNAME')]
 
 # Application definition
 
@@ -98,8 +98,8 @@ if development:
         }
     }
 else:
-    #DATABASES = { 'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
-    DATABASES = { 'default': dj_database_url.parse("postgres://wpijjjmavujnkq:e74bd851b04e5f8f0a105ed9362e797b7e89b4648e30fa84912469f5a06d02ae@ec2-54-217-225-16.eu-west-1.compute.amazonaws.com:5432/d8jj5e5ru177b")}
+    DATABASES = { 'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+#DATABASES = { 'default': dj_database_url.parse("postgres://wpijjjmavujnkq:e74bd851b04e5f8f0a105ed9362e797b7e89b4648e30fa84912469f5a06d02ae@ec2-54-217-225-16.eu-west-1.compute.amazonaws.com:5432/d8jj5e5ru177b")}
 
 
 
